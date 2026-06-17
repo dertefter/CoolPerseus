9b01e1e17de333fbf9ed7d30c8aaebedf417e213
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2026-06-17 09:55:45 +0700

    service.sh: install apk if missing

--- service.sh
+++ service.sh
@@ -1,12 +1,17 @@
 #!/system/bin/sh
 
 PKG="com.dertefter.coolperseus"
+APK_PATH="/system/priv-app/CoolPerseus/CoolPerseus.apk"
 
 until [ "$(getprop sys.boot_completed)" = "1" ]; do
     sleep 5
 done
 
 sleep 10
+
+if ! pm list packages | grep -q "$PKG"; then
+    pm install -r -g "$APK_PATH"
+fi
 
 appops set $PKG SYSTEM_ALERT_WINDOW allow
 appops set $PKG RUN_ANY_IN_BACKGROUND allow
